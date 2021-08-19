9b2446701457859abf51e64e1520913bcb1cec76
ShivanshiModi <87198158+ShivanshiModi@users.noreply.github.com>
2021-08-19 22:50:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+    var name1 = "Shivanshi ";
+    console.log("First Name: "+name1);
+    var name2 = "Modi";
+    console.log("Last Name:"+name2);
+    var full_name = name1.concat(name2);
+    console.log("Full Name:"+full_name);
+    
+    var i = 6;
+    console.log("a="+i);
+    var j = 4;
+    console.log("b="+j);
+    var k =  i+j;
+    console.log("a+b="+k);
+
+var d = j - i;
+console.log("b-a="+d);
+var e = i*j;
+console.log("a*b="+e);
+var f =  j/i;
+console.log("b/a="+f);
